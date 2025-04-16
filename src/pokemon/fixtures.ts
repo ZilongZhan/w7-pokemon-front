0db94a598aa498c8ecfc51de785ae0d2ccbb21d8
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-16 03:35:42 +0200

    Add fixture pokemons

---
+++ src/pokemon/fixtures.ts
@@ -0,0 +1,19 @@
+import { Pokemon } from "./types";
+
+const plusle: Pokemon = {
+  id: "1",
+  pokeIndex: 311,
+  name: "Plusle",
+  imageUrl: "http://plusle.com/plusle.webp",
+  isCaptured: false,
+};
+
+const minun: Pokemon = {
+  id: "2",
+  pokeIndex: 312,
+  name: "Minun",
+  imageUrl: "http://minun.com/minun.webp",
+  isCaptured: false,
+};
+
+export { plusle, minun };
