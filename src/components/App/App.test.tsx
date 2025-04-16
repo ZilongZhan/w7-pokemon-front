cb354e972c19ac3465f5beba25c8daa11b63bb1d
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-16 04:10:02 +0200

    Fix component render context in tests

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
 import App from "./App";
 
 describe("Given the App component", () => {
   describe("When it renders", () => {
     test("Then it should show 'PokéManager' inside a level 1 heading", () => {
-      render(<App />);
+      render(<App />, { wrapper: MemoryRouter });
 
       const appTitle = screen.getByRole("heading", { name: /pokémanager/i });
 
