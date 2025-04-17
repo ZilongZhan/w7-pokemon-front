8ba8f1d79c7a4e9240980d1a1e6351e77488c419
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-17 03:29:58 +0200

    Modify Pokemon interface

--- src/pokemon/types.ts
+++ src/pokemon/types.ts
@@ -1,7 +1,31 @@
+export type Type =
+  | "normal"
+  | "fire"
+  | "water"
+  | "electric"
+  | "grass"
+  | "ice"
+  | "fighting"
+  | "poison"
+  | "ground"
+  | "flying"
+  | "psychic"
+  | "bug"
+  | "rock"
+  | "ghost"
+  | "dragon"
+  | "dark"
+  | "steel"
+  | "fairy";
+
 export interface Pokemon {
   id: string;
   pokeIndex: number;
   name: string;
+  types: Type[];
+  abilities: string[];
   imageUrl: string;
   isCaptured: boolean;
 }
+
+export type PokemonDetails = Pick<Pokemon, "abilities" | "types">;
