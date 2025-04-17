eadd58e5d6f93426078b21b9834040cca2b4c699
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-17 07:07:49 +0200

    Implement PokemonContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import AppRouter from "./router/AppRouter";
 import { BrowserRouter } from "react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import AppRouter from "./router/AppRouter";
+import PokemonContextProvider from "./pokemon/context/PokemonContextProvider";
 
 import "@fontsource/press-start-2p/index.css";
 import "./styles/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <PokemonContextProvider>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </PokemonContextProvider>
   </StrictMode>,
 );
--- src/pokemon/pages/PokemonHomePage/PokemonHomePage.tsx
+++ src/pokemon/pages/PokemonHomePage/PokemonHomePage.tsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
-import { Pokemon } from "../../types";
+import { useEffect } from "react";
+import PokemonCardsList from "../../components/PokemonCardsList/PokemonCardsList";
+import usePokemons from "../../hooks/usePokemons";
 
 const PokemonHomePage: React.FC = () => {
-  const [pokemons] = useState<Pokemon[]>([]);
+  const { pokemons, renderPokemons } = usePokemons();
+
+  useEffect(() => {
+    renderPokemons();
+  }, [renderPokemons]);
 
   return (
     <>
       <h2>Home</h2>
       <span>Total Pokémon: {pokemons.length}</span>
+      <PokemonCardsList pokemons={pokemons} />
     </>
   );
 };
