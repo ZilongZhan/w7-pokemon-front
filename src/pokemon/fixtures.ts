8912a83e4590a0e7a22fd8677fe91ea8919df663
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-17 03:34:54 +0200

    Upate fixture pokemon declarations

--- src/pokemon/fixtures.ts
+++ src/pokemon/fixtures.ts
@@ -4,6 +4,8 @@ const plusle: Pokemon = {
   id: "1",
   pokeIndex: 311,
   name: "Plusle",
+  types: ["electric"],
+  abilities: ["Plus", "Lightning Rod"],
   imageUrl: "http://plusle.com/plusle.webp",
   isCaptured: false,
 };
@@ -11,6 +13,8 @@ const plusle: Pokemon = {
 const minun: Pokemon = {
   id: "2",
   pokeIndex: 312,
+  types: ["electric"],
+  abilities: ["Minus", "Volt absorb"],
   name: "Minun",
   imageUrl: "http://minun.com/minun.webp",
   isCaptured: false,
