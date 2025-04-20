ab8acacdbbc893792d582bbbbfc9144acc3cc5ad
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-20 21:58:08 +0200

    Add missing render wrapper in component tests

--- src/pokemon/components/PokemonCardsList/PokemonCardsList.test.tsx
+++ src/pokemon/components/PokemonCardsList/PokemonCardsList.test.tsx
@@ -1,13 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import PokemonCardsList from "./PokemonCardsList";
 import { minun, plusle } from "../../fixtures";
+import PokemonContextProvider from "../../context/PokemonContextProvider";
 
 describe("Given the PokemonCardsList component", () => {
   describe("When it receives Plusle and Minun", () => {
     test("Then it should show 'Plusle' and 'Minun' inside a heading each", () => {
       const pokemons = [plusle, minun];
 
-      render(<PokemonCardsList pokemons={pokemons} />);
+      render(<PokemonCardsList pokemons={pokemons} />, {
+        wrapper: PokemonContextProvider,
+      });
 
       const plusleName = screen.getByRole("heading", { name: /plusle/i });
       const minumName = screen.getByRole("heading", { name: /minun/i });
